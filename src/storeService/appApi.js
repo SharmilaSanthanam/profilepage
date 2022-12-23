0cc97203754793b2746b2bd4159be244a4b18889
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2022-12-24 00:04:01 +0530

    Update appApi.js

--- src/storeService/appApi.js
+++ src/storeService/appApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const appApi = createApi({
     reducerPath: "appApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5001" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://profile-zvxb.onrender.com" }),
     endpoints: (builder) => ({
         signup: builder.mutation({
             query: (user) => ({
